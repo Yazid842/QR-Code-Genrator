938b1f624ee78480fd107d5f7729de7efe18df0c
yazid najim <160153111+Yazid842@users.noreply.github.com>
2025-07-12 18:15:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let qrImage= document.getElementById("qrImage")
 let imgBOx= document.getElementById("imgBox")
 let qrText= document.getElementById("qrText")
+let btnD= document.getElementById("Download")
 
-function genrateQR(){
+function genrateQR(btn){
     if(qrText.value!=""){
-        qrImage.src=" https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +qrText.value;
+        qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +qrText.value;
+        btn.removeAttribute("style");
+        btnD.style.display = "inline-flex";
         imgBOx.classList.add("show-img");
     }else{
         qrText.setAttribute('style',"border: 1px solid red;");
@@ -15,4 +18,23 @@ function genrateQR(){
 
         },600);
     }
+}
+function Download(){
+    const imgsrc=qrImage.src;
+    if(!qrImage){
+        return ;
+    }
+    fetch(imgsrc)
+    .then(resultat=>resultat.blob())
+    .then(blob=>{
+        const url=URL.createObjectURL(blob);
+        const link=document.createElement("a");
+        link.href=url;
+        link.download="QR.png";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
+    })
+    .catch(()=>alert("Erreur dans Telachargement"));
 }
